a7ce6c4ea71500d47cfdd411403fb642090ceab7
abdulmateensanusi <abdulmateensanusi@gmail.com>
2023-03-16 16:50:48 -0700

    advanved task (8)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: 1 when successful
+ */
+int main(void)
+{
+	char *mssg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+	write(2, message, 43);
+
+	return (1);
+}
