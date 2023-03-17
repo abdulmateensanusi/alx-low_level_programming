f1d1340ddfb9a2daba201d9e69d45b090e0bb2cd
abdulmateensanusi <abdulmateensanusi@gmail.com>
2023-03-17 16:39:37 -0700

    printing out the alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *alp = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (alp[i] != '\0')
+	{
+		putchar(alp[i]);
+		i++;
+	}
+
+	return (0);
+}
