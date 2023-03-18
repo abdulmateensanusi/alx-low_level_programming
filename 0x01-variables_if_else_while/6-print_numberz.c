0248f178da680783715d1e02b4f71064503336aa
abdulmateensanusi <abdulmateensanusi@gmail.com>
2023-03-17 17:35:25 -0700

    base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a);
+		a++;
+	}
+
+	return (0);
+}
