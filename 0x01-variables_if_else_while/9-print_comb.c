409db4fe0e1606658677f5cde76ade91be13cd8f
abdulmateensanusi <abdulmateensanusi@gmail.com>
2023-03-17 18:17:02 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
