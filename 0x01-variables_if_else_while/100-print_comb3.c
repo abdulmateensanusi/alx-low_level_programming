86098418ad5945424dffc89392bc5656856df95d
abdulmateensanusi <abdulmateensanusi@gmail.com>
2023-03-19 14:36:06 -0700

    combination of 2 digits numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,22 +3,26 @@
 /**
  * main - Entry point
  *
- * Return - Always 0 (Success)
+ * Return:  Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++){
-		for (j = i + 1; j < 10; j++){
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if (i != 8 || j!= 9){
+			if (i != 8 || j != 9)
+			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+	putchar('\n');
+
 	return (0);
 }
