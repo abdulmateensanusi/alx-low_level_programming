43ab1481997fb93b0cf9c227d9b7400d60eb3623
abdulmateensanusi <abdulmateensanusi@gmail.com>
2023-03-21 21:39:12 -0700

    task 0 - printing "_putchar"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include <main.h>
 
+/* function declaration*/
+int _putchar(char c);
 /**
  * main -Entry point
  *
@@ -8,7 +10,15 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
