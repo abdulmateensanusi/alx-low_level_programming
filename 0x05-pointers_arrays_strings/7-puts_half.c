9b3d0d6bbfad995db8548b88ccad0cd9da4e306b
abdulmateensanusi <abdulmateensanusi@gmail.com>
2023-03-28 09:53:16 -0700

    task 7 and task 8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints every the second half of a string
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	char *s_original = str;
+	int middle;
+	int length = 0;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+	str = s_original;
+	if (length % 2 == 0)
+	{
+		for (middle = length / 2; middle <= length; middle++)
+		{
+			_putchar(str[middle]);
+		}
+	}
+	else
+	{
+		for (middle = (length - 1) / 2; middle <= length; middle++)
+		{
+			_putchar(str[middle]);
+		}
+	}
+	_putchar('\n');
+}
