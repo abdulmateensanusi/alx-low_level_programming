e339b401cb8f571c2a55ecae60fd69719f5c5dce
abdulmateensanusi <abdulmateensanusi@gmail.com>
2023-04-03 22:32:46 -0700

    task 2 - _strchr()

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stddef.h>
+
 /**
  * _strchr - Returns a pointer to the first occurrence of the
  * character c in the string s, or NULL if the character is not found
@@ -6,9 +8,10 @@
  * @c: character to check
  * Return: a pointer to s or NULL
  */
+
 char *_strchr(char *s, char c)
 {
-	char *match = "NULL";
+	char *match = NULL;
 
 	while (*s != '\0')
 	{
